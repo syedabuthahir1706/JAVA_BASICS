a791a9f3c7e2464046d7ad149aedc77fbf8fd66e
Dell <Dell@DESKTOP-P4S8DMH>
2020-11-10 13:10:39 +0800

    added class arithmetic_operation

---
+++ JAVA_BASICS/src/arithmetic_operation.java
@@ -0,0 +1,16 @@
+
+
+public class arithmetic_operation {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=10; b=20;
+	    System.out.println("add :"+(a+b));
+		System.out.println("sub :"+(a-b));
+		System.out.println("mul :"+(a*b));
+		System.out.println("div :"+(a/b));
+		System.out.println("mod :"+(a%b));
+		
+	}
+
+}
